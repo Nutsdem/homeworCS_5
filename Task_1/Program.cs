69b1d66c3b1a5d5dc6729ed9967617e783452e2d
nutsdem <nechaeff91@gmail.com>
2022-08-08 17:12:33 +0500

    Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,19 +1,19 @@
 ﻿int[] numbers = new int[10];
 
-void FillArray(int[] array, int min, int max){
+void fillArray(int[] array, int min, int max){
   for (int i = 0; i<array.Length; i++ ){
     array[i] = new Random().Next(min, max);
   }
 }
 
-void WriteArray(int[] array){
+void writeArray(int[] array){
     for (int i = 0; i<array.Length; i++ ){
     Console.Write(array[i] + " ");
   }
   Console.WriteLine();
 }
 
-int QuantityPositive(int[] array){
+int quantityPositive(int[] array){
     int quantity = 0;
     for (int i = 0; i<array.Length; i++ ){
     if (array[i] % 2 == 0)
@@ -24,9 +24,9 @@ int QuantityPositive(int[] array){
   return quantity;
 }
 
-FillArray(numbers, 100, 1000);
-WriteArray(numbers);
+fillArray(numbers, 100, 1000);
+writeArray(numbers);
 Console.WriteLine();
 
-int quantity = QuantityPositive(numbers);
+int quantity = quantityPositive(numbers);
 Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
